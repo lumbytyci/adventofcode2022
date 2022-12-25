b344037f94f9f358e6770365cecceaeec6e836bf
Lumi Bytyci <lumbytyci@hotmail.com>
2022-12-25 21:11:52 +0100

    Add day06

---
+++ day06/Part1.cs
@@ -0,0 +1,36 @@
+﻿namespace AOC2022_Day6;
+
+public static class Part1
+{
+    public static void Main(string[] args)
+    {
+        var input = @"mjqjpqmgbljsphdztnvjfqwrcgsmlb";
+
+        /* Run with sample input if any argument is supplied */
+        if (args.Length == 1)
+        {
+            input = File.ReadAllText("./input.txt");
+        }
+
+        var watch = new System.Diagnostics.Stopwatch();
+
+        watch.Start();
+
+        var result = RunPart(input);
+        Console.WriteLine($"Result: {result}. Execution time: {watch.ElapsedMilliseconds} ms");
+
+        Console.ReadKey();
+    }
+
+    public static int RunPart(string input)
+    {
+        var stream = input.AsSpan();
+        for (int i = 0; i <= stream.Length - 4; i++)
+        {
+            var markerSpan = stream.Slice(i, 4);
+            if (markerSpan.ToArray().Distinct().Count() == 4) return i + 4;
+        }
+
+        return default;
+    }
+}
---
+++ day06/Part2.cs
@@ -0,0 +1,37 @@
+﻿namespace AOC2022_Day6;
+
+public static class Part2
+{
+    public static void Main(string[] args)
+    {
+        var input = @"mjqjpqmgbljsphdztnvjfqwrcgsmlb";
+
+        /* Run with sample input if any argument is supplied */
+        //if (args.Length == 1)
+        {
+            input = File.ReadAllText("./input.txt");
+        }
+
+        var watch = new System.Diagnostics.Stopwatch();
+
+        watch.Start();
+
+        var result = RunPart(input);
+        Console.WriteLine($"Result: {result}. Execution time: {watch.ElapsedMilliseconds} ms");
+
+        Console.ReadKey();
+    }
+
+    public static int RunPart(string input)
+    {
+        const int markerLength = 14;
+        var stream = input.AsSpan();
+        for (int i = 0; i <= stream.Length - markerLength; i++)
+        {
+            var markerSpan = stream.Slice(i, markerLength);
+            if (markerSpan.ToArray().Distinct().Count() == markerLength) return i + markerLength;
+        }
+
+        return default;
+    }
+}
